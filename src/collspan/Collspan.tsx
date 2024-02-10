eac38d323c2d1830a591a3198d80c234bd038ae4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-10 07:36:59 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/collspan/Collspan.tsx
+++ src/collspan/Collspan.tsx
@@ -3,7 +3,10 @@ const Collspan = () => {
     <div className="w-full h-screen bg-gradient-to-r from-indigo-500 to-blue-600">
       <div className="container mx-auto px-40 py-12">
         <div className="bg-white text-xl px-12 py-8 rounded">
-          <p className="">Hi all</p>
+          <div className="">
+            <p className="">This is title</p>
+            <p className="">+</p>
+          </div>
         </div>
       </div>
     </div>
