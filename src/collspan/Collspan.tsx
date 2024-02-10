0a0b50890a862737b7c00fb96dc9246ed1c2b335
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-10 07:38:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/collspan/Collspan.tsx
+++ src/collspan/Collspan.tsx
@@ -3,7 +3,7 @@ const Collspan = () => {
     <div className="w-full h-screen bg-gradient-to-r from-indigo-500 to-blue-600">
       <div className="container mx-auto px-40 py-12">
         <div className="bg-white text-xl px-12 py-8 rounded">
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full font-semibold">
             <p className="">This is title</p>
             <p className="">+</p>
           </div>
